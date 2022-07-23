d7f541368862b75532a15fad6bcea3e29f4298f8
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-07-23 13:38:48 +0900

    feat : CORS 설정

--- src/main/java/dnd/studyplanner/config/WebMvcConfig.java
+++ src/main/java/dnd/studyplanner/config/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package dnd.studyplanner.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,8 +15,25 @@ import lombok.RequiredArgsConstructor;
 public class WebMvcConfig implements WebMvcConfigurer {
 	private final JwtTokenInterceptor jwtTokenInterceptor;
 
+	@Value("${front-client.domain}")
+	public String DOMAIN_HOST;
+
+
 	public void addInterceptors(InterceptorRegistry registry) {
 		//Interceptor scope
 		registry.addInterceptor(jwtTokenInterceptor).addPathPatterns("/token/test/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+			.addMapping("/**")
+			.allowedOrigins(DOMAIN_HOST)
+			.allowedMethods(
+				HttpMethod.GET.name(),
+				HttpMethod.POST.name(),
+				HttpMethod.PATCH.name(),
+				HttpMethod.DELETE.name()
+			);
 	}
 }
