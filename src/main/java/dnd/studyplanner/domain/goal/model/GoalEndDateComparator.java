4b0e52064c4a1ab183a21266d775217a08c7eac2
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-09-09 20:48:13 +0900

    feat : 스터디 그룹 조회 시 세부 목표 정렬을 위한 Comparator 클래스 수정

--- src/main/java/dnd/studyplanner/domain/goal/model/GoalEndDateComparator.java
+++ src/main/java/dnd/studyplanner/domain/goal/model/GoalEndDateComparator.java
@@ -1,14 +1,16 @@
 package dnd.studyplanner.domain.goal.model;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 
-import static java.time.Period.between;
-
 public class GoalEndDateComparator implements Comparator<Goal> {
+
     @Override
     public int compare(Goal o1, Goal o2) {
         LocalDate today = LocalDate.now();
-        return String.valueOf(between(today, o1.getGoalEndDate()).getDays()).compareTo(String.valueOf(between(today, o2.getGoalEndDate())));
+
+        return String.valueOf(today.until(o1.getGoalEndDate(), ChronoUnit.DAYS))
+                .compareTo(String.valueOf(today.until(o2.getGoalEndDate(), ChronoUnit.DAYS)));
     }
 }
